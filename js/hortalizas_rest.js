f66299176cf329c9f7dc560b980efe6202112752
FernandoTelloL <fernando_tellol@hotmail.com>
2021-04-25 19:41:28 -0500

    ultimos cambios en hortalizas

--- js/hortalizas_rest.js
+++ js/hortalizas_rest.js
@@ -1,24 +1,26 @@
-// self executing function here
-(function () {
-  // your page initialization code here
-  // the DOM will be available here
+window.onload = () => {
   getProductsByCategory();
-})();
+};
 
-function getParamFromUrl(key) {
+const getParamFromUrl = key => {
   let url = window.location.search;
   let urlParams = new URLSearchParams(url);
   return urlParams.get(key);
-}
+};
 
 async function getProductsByCategory() {
   const id = getParamFromUrl('id');
+
   let div = document.getElementById('vegetables-card-container');
+
   const response = await fetch(
     `http://localhost:8090/api/producto/getProductsByCategory/${id}`,
   );
+
   let data = await response.json();
+
   let items = '';
+
   data.forEach(r => {
     items += ` <div class="vegetables-card">
             <div class="vegetables-card__container-image">
@@ -29,7 +31,16 @@ async function getProductsByCategory() {
               />
             </div>
             <div class="vegetables-card1__text">
-              <a class="button button--vegetables-card" id="open">comprar</a>
+              <a 
+                class="button button--vegetables-card" 
+                id="open" 
+                onclick="modalPage()" 
+                href="#?id=${r.id_categoriaProduc}"
+                >
+                comprar
+                </a>
+              
+                    
               <h2 class="vegetables-card__title">${r.nombre_producto}</h2>
               <p class="vegetables-card__content">${r.detalle_producto}</p>
             </div>
@@ -50,40 +61,123 @@ async function getProductsByCategory() {
               </div>
             </div>
           </div>
+          
+          
+          <!-- ------------- First modal ------------ -->
+          <div class="modal-container modal-container1" id="modal_container">
+            <div class="modal">
+              <h1 class="modal__title">Bolsa de Compras</h1>
+              <div class="modal__info-buy">
+                <div class="modal__image">
+                  <img src="${r.imagenProducto}" alt="" />
+                </div>
+                <div class="modal_information-buy">
+                  <p class="product-name">${r.nombre_producto}</p>
+                  <p class="product-code">${r.codigo_producto}</p>
+                  <a
+                    href="./productos_por_categoria.html"
+                    class="delete-product"
+                    >Eliminar</a
+                  >
+                </div>
+              </div>
+
+              <div class="modal__cost">
+                <div class="modal__total-cost">
+                  <div class="modal__price">
+                    <div class="modal__content">Precio</div>
+                    <div class="modal__price" id="modal__price">2</div>
+                  </div>
+
+                  <div class="modal__sub-total">
+                    <div class="modal__content">Subtotal</div>
+                    <div class="modal__monto" id="modal__montosub"></div>
+                  </div>
+
+                  <div class="modal__descuento">
+                    <div class="modal__content">Descuento</div>
+                    <div
+                      class="modal__montoDescuento"
+                      id="modal__montoDescuento"
+                    >
+                      2
+                    </div>
+                  </div>
+
+                  <div class="modal__total">
+                    <div class="modal__content">Total</div>
+                    <div class="modal__montoTotal" id="modal__montoTotal">
+                      0
+                    </div>
+                  </div>
+                </div>
+
+                <div class="modal__count">
+                  <h3 class="modal__count-title">CANTIDAD</h3>
+                  <button class="modal__decrease" id="modal-decrease">-</button>
+                  <input
+                    type="text"
+                    name="modal__count-value"
+                    class="modal__count-value"
+                    readonly
+                    value="1"
+                  />
+                  <button class="modal__increment" id="modal-increment">
+                    +
+                  </button>
+                </div>
+
+                <div class="modal__offered-container">
+                  <div class="modal__offered-yes">Disponible</div>
+                  <div class="modal__offered-no hide">No Disponible</div>
+                </div>
+              </div>
+
+              <div class="modal__resume">
+                <h3 class="modal__resume-title">Resumen de tu orden</h3>
+                <div class="modal__resume-section1">
+                  <div class="modal__resume-subtotal">
+                    <div class="modal__resume-subtotal-text">
+                      Subtotal productos
+                    </div>
+                    <div
+                      class="modal__resume-subtotal-mount"
+                      id="modal__resume-subtotal-mount"
+                    ></div>
+                  </div>
+
+                  <div class="modal__resume-descuento">
+                    <div class="modal__resume-descuento-text">Descuento</div>
+                    <div
+                      class="modal__resume-descuento-total"
+                      id="modal__resume-descuento-total"
+                    ></div>
+                  </div>
+
+                  <div class="modal__resume-total">
+                    <div class="modal__resume-total-text">Total a pagar</div>
+                    <div
+                      class="modal__resume-total-total"
+                      id="modal__resume-total-total"
+                    ></div>
+                  </div>
+                </div>
+                <div class="modal__resume-section2">
+                  <div class="buttons-buy">
+                    <button class="button modal__button-comprar" id="">
+                      comprar
+                    </button>
+                    <button class="button modal__button-cerrar btnClose" onclick="closeModal()">
+                      cerrar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- --------X----- End First modal ------X-------->
  `;
   });
 
   div.innerHTML = items;
-  // getUserPost();
 }
-
-/*
-
-async function getProductsByCategory() {
-  let cardByCategoryContainer = document.getElementById(
-    'vegetables-card-container',
-  );
-  const response = await fetch(
-    'http://localhost:8090/api/producto/getCategoryProducts',
-  );
-  let data = await response.json();
-  let items = '';
-  data.forEach(r => {
-    cardCategory.innerHTML += `
-          <div class="produce-section2__category">
-            <img
-                id="img-category"
-                class="img-category"
-                src="${r.imagen_categoria}"
-                alt="img-category"
-            />
-            <h2 class="produce-section2__categories-title">${r.nombre_categoria}</h2>
-            <a
-                class="button button--section2-categories"
-                href="productos_por_categoria.html"
-            >Ver más
-            </a>
-          </div>`;
-  });
-}
-*/
--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,10 @@
-const open = document.getElementById('open'),
-  modal_container = document.getElementById('modal_container'),
+/*const openModal = document.getElementById('open')*/
 
-open.addEventListener('click', () => {
+const modalPage = () => {
+  const modal_container = document.getElementById('modal_container');
   modal_container.classList.add('show');
-});
-/*
-
-open2.addEventListener('click', () => {
-  modal2_container.classList.add('show');
-});
-
-open3.addEventListener('click', () => {
-  modal3_container.classList.add('show');
-});
-
-close1.addEventListener('click', () => {
+};
+const closeModal = () => {
+  const modal_container = document.getElementById('modal_container');
   modal_container.classList.remove('show');
-});
-
-close2.addEventListener('click', () => {
-  modal2_container.classList.remove('show');
-});
-
-close3.addEventListener('click', () => {
-  modal3_container.classList.remove('show');
-});
-*/
+};
